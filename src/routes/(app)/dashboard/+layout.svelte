7b2e39d4a927133403c5d936c24dc3e0c9615a64
Isaac Wassouf <isaacwassouf@gmail.com>
2023-10-28 01:05:41 +0200

    fix: set the right links in the dropdown and side menu

--- src/routes/(app)/dashboard/+layout.svelte
+++ src/routes/(app)/dashboard/+layout.svelte
@@ -96,9 +96,8 @@
 							<span class="block text-sm text-gray-900 dark:text-white">{$userStore.name}</span>
 							<span class="block truncate text-sm font-medium">{$userStore.email}</span>
 						</div>
-						<DropdownItem>Dashboard</DropdownItem>
+						<DropdownItem href="/dashboard">Dashboard</DropdownItem>
 						<DropdownItem href="/dashboard/account">Account</DropdownItem>
-						<DropdownItem>Earnings</DropdownItem>
 						<DropdownItem slot="footer" on:click={() => logoutUser()}>Sign out</DropdownItem>
 					</Dropdown>
 				</div>
@@ -116,7 +115,7 @@
 		<ul class="space-y-2 font-medium">
 			<li>
 				<a
-					href="#"
+					href="/dashboard"
 					class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
 				>
 					<svg
@@ -138,25 +137,13 @@
 			</li>
 			<li>
 				<a
-					href="#"
+					href="/dashboard/challenges"
 					class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
 				>
-					<svg
-						class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-						aria-hidden="true"
-						xmlns="http://www.w3.org/2000/svg"
-						fill="currentColor"
-						viewBox="0 0 18 18"
-					>
-						<path
-							d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z"
-						/>
-					</svg>
-					<span class="flex-1 ml-3 whitespace-nowrap">Kanban</span>
-					<span
-						class="inline-flex items-center justify-center px-2 ml-3 text-sm font-medium text-gray-800 bg-gray-100 rounded-full dark:bg-gray-700 dark:text-gray-300"
-						>Pro</span
-					>
+				<svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 17 20">
+					<path d="M7.958 19.393a7.7 7.7 0 0 1-6.715-3.439c-2.868-4.832 0-9.376.944-10.654l.091-.122a3.286 3.286 0 0 0 .765-3.288A1 1 0 0 1 4.6.8c.133.1.313.212.525.347A10.451 10.451 0 0 1 10.6 9.3c.5-1.06.772-2.213.8-3.385a1 1 0 0 1 1.592-.758c1.636 1.205 4.638 6.081 2.019 10.441a8.177 8.177 0 0 1-7.053 3.795Z"/>
+				</svg>
+					<span class="flex-1 ml-3 whitespace-nowrap">Challenges</span>
 				</a>
 			</li>
 			<li>
