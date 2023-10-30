74a55f55aebf4a697a497a0f9669937ce05e233d
Isaac Wassouf <isaacwassouf@gmail.com>
2023-10-30 20:28:22 +0100

    feat: add logic for next and repeat buttons

--- src/routes/(app)/dashboard/challenges/standard/+page.svelte
+++ src/routes/(app)/dashboard/challenges/standard/+page.svelte
@@ -23,6 +23,7 @@
 	let duration: number = 0;
 	let accuracy: number = 0;
 	let wordsPerMinute: number = 0;
+	let loading: boolean = false;
 
 	const registerMistake = (correct: string, mistake: string) => {
 		// if a mistake for this word exist push the new mistake
@@ -86,6 +87,54 @@
 		inputValue = '';
 	};
 
+	const handleNext = async () => {
+		modalOpen = false;
+		loading = true;
+
+		// fetch an new quote
+		const response: Response = await fetch('https://api.quotable.io/random');
+
+		if (response.ok) {
+			// parse the json data
+			quote = await response.json();
+
+			// reset the variables
+			words = quote.content.split(' ');
+			activeWordIndex = 0;
+			inputValue = '';
+			mistakes = new Map<string, string[]>();
+			firstInteractionWithInput = true;
+			start = 0;
+			end = 0;
+			duration = 0;
+			accuracy = 0;
+			wordsPerMinute = 0;
+
+			inputField.focus();
+		}else{
+			console.log('error fetching new quote', response);
+		}
+
+		loading = false;
+	};
+
+	const handleRepeat = () => {
+		modalOpen = false;
+
+		// reset the variables
+		activeWordIndex = 0;
+		inputValue = '';
+		mistakes = new Map<string, string[]>();
+		firstInteractionWithInput = true;
+		start = 0;
+		end = 0;
+		duration = 0;
+		accuracy = 0;
+		wordsPerMinute = 0;
+
+		inputField.focus();
+	};
+
 	// run the handler whenever the inputValue changes
 	$: handleInput(inputValue);
 
@@ -94,33 +143,46 @@
 	});
 </script>
 
-<ResultModal bind:modalOpen {wordsPerMinute} {duration} {accuracy} on:next={() => console.log('next')} on:repeat={() => console.log('repeat')} />
+<ResultModal bind:modalOpen {wordsPerMinute} {duration} {accuracy} on:next={handleNext} on:repeat={handleRepeat} />
 
-<div class="flex justify-around items-center flex-col h-full dark:bg-gray-800">
-	<div>
-		<Card class="text-center mx-8" size="xl" padding="xl">
-			<div
-				class="mb-2 flex flex-wrap justify-center items-center gap-1.5 text-2xl font-bold text-gray-500 dark:text-gray-400"
-			>
-				{#each words as word, ind}
-					<Word {word} active={activeWordIndex === ind} />
-				{/each}
-			</div>
-
-			<div class="flex justify-end text-sm italic text-gray-900 dark:text-gray-400">
-				{quote.author}
-			</div>
-		</Card>
+{#if loading}
+	<div class="h-full flex justify-center items-center">
+		<div role="status">
+			<svg aria-hidden="true" class="inline w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-sky-700" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+				<path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+				<path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+			</svg>
+			<span class="sr-only">Loading...</span>
+		</div>
 	</div>
+{:else}
+	<div class="flex justify-around items-center flex-col h-full dark:bg-gray-800">
+		<div>
+			<Card class="text-center mx-8" size="xl" padding="xl">
+				<div
+					class="mb-2 flex flex-wrap justify-center items-center gap-1.5 text-2xl font-bold text-gray-500 dark:text-gray-400"
+				>
+					{#each words as word, ind}
+						<Word {word} active={activeWordIndex === ind} />
+					{/each}
+				</div>
 
-	<input
-		type="text"
-		id="input-word"
-		bind:this={inputField}
-		bind:value={inputValue}
-		on:keydown={startTimerOnFirstInteraction}
-		class="w-40 h-16 border text-center text-lg font-bold text-gray-900 rounded-lg block p-2.5
-			bg-gray-50 border-gray-300 focus:ring-sky-700 focus:border-sky-700
-			dark:bg-gray-800 dark:border-gray-600 dark:text-white dark:focus:ring-sky-700 dark:focus:border-sky-700"
-	/>
-</div>
+				<div class="flex justify-end text-sm italic text-gray-900 dark:text-gray-400">
+					{quote.author}{'.'}
+				</div>
+			</Card>
+		</div>
+
+		<input
+			type="text"
+			id="input-word"
+			bind:this={inputField}
+			bind:value={inputValue}
+			on:keydown={startTimerOnFirstInteraction}
+			class="w-40 h-16 border text-center text-lg font-bold text-gray-900 rounded-lg block p-2.5
+				bg-gray-50 border-gray-300 focus:ring-sky-700 focus:border-sky-700
+				dark:bg-gray-800 dark:border-gray-600 dark:text-white dark:focus:ring-sky-700 dark:focus:border-sky-700"
+		/>
+	</div>
+{/if}
+
